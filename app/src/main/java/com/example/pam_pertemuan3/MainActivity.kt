76e3e68be4c5dab08374dd60d95aace232d6d731
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-18 17:17:58 +0700

    menambahkan underline pada textnya

--- app/src/main/java/com/example/pam_pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pam_pertemuan3/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -76,7 +77,8 @@ fun BasicCompose(guntur: Modifier = Modifier){
         Text(text = "20220140002",
             fontWeight = FontWeight.Bold,
             fontSize = 50.sp,
-            fontFamily = FontFamily.Monospace
+            fontFamily = FontFamily.Monospace,
+            textDecoration = TextDecoration.Underline
             )
     }
 }
